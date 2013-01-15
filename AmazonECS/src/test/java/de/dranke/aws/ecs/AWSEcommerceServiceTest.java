8bbf46ebe8ef202665131d5936a34ee3a10bb61c
Daniel Ranke <daniel.ranke@hotmail.de>
2013-01-15 21:08:36 +0100

    [AWS] listing some fields of the result items

--- AmazonECS/src/test/java/de/dranke/aws/ecs/AWSEcommerceServiceTest.java
+++ AmazonECS/src/test/java/de/dranke/aws/ecs/AWSEcommerceServiceTest.java
@@ -1,8 +1,12 @@
 package de.dranke.aws.ecs;
 
 import com.amazon.ecs.client.jax.*;
+import com.sun.deploy.util.StringUtils;
 import de.dranke.aws.security.AwsSecurityHandler;
 import de.dranke.aws.security.AwsSecurityProperties;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.TransformerUtils;
 import org.apache.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -15,9 +19,11 @@ import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static de.dranke.aws.security.AwsSoapAction.ITEM_SEARCH;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.fest.assertions.Assertions.assertThat;
 
 /**
@@ -79,7 +85,55 @@ public class AWSEcommerceServiceTest {
     assertThat(response.getItems()).isNotEmpty();
 
     for (Items item : response.getItems()) {
-      LOG.debug(String.format("Item gefunden: %d", item.getTotalResults()));
+      LOG.debug(String.format("Items gefunden: %d", item.getTotalResults()));
+      int itemCount = 0;
+      for (Item resultItem : item.getItem()) {
+        LOG.debug(String.format("Item content %d: %s", ++itemCount, itemContent(resultItem)));
+
+      }
+    }
+  }
+
+  private String itemContent(Item resultItem) {
+    StringBuilder builder = new StringBuilder();
+    append("ASIN", resultItem.getASIN(), builder);
+//    append(resultItem.getDetailPageURL(), "DetailPage", builder);
+    if (resultItem.getOfferSummary() != null) {
+      append("TotalCollectible", resultItem.getOfferSummary().getTotalCollectible(), builder);
+    }
+    append("ItemAttributes", EMPTY, builder);
+    append(resultItem.getItemAttributes(), builder);
+
+    return builder.toString();
+  }
+
+  private void append(ItemAttributes itemAttributes, StringBuilder builder) {
+    builder.append("\n\t\t\t" + "Title : " + itemAttributes.getTitle());
+    builder.append("\n\t\t\t" + "Author : " + StringUtils.join(itemAttributes.getAuthor(), ", "));
+    builder.append("\n\t\t\t" + "Categories : " + StringUtils.join(itemAttributes.getCategory(), ", "));
+    Collection transformCollection = CollectionUtils.transformedCollection(new ArrayList(), new CreatorTransformer());
+    transformCollection.addAll(itemAttributes.getCreator());
+    builder.append("\n\t\t\t" + "Creator : " + StringUtils.join(transformCollection, ", "));
+    builder.append("\n\t\t\t" + "ISBN : " + itemAttributes.getISBN());
+
+    if (itemAttributes.getListPrice() != null) {
+      builder.append("\n\t\t\t" + "List price : " + itemAttributes.getListPrice().getFormattedPrice());
+    }
+  }
+
+  private void append(String field, String value, StringBuilder builder) {
+    builder.append("\n\t\t" + field + " : " + value);
+  }
+
+  class CreatorTransformer implements Transformer {
+
+    @Override
+    public Object transform(Object object) {
+      if (object instanceof ItemAttributes.Creator) {
+        ItemAttributes.Creator creator = (ItemAttributes.Creator) object;
+        return creator.getValue() + " (Role: " + creator.getRole() + ")";
+      }
+      return null;
     }
   }
 
